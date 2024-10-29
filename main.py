6871323a777118c2b506d416d4e8c2600d34cd2f
Ivan Shmikk <shmikk.v@yandex.ru>
2024-10-29 20:36:34 +0700

    change task 1

--- main.py
+++ main.py
@@ -4,18 +4,20 @@ class FlatIterator:
 		self.list_of_list = list_of_list
 
 	def __iter__(self):
-		self.cursor = -1
+		self.cursor = 0
 		self.nested_cursor = -1
 		return self
 
 	def __next__(self):
 		self.nested_cursor += 1
+		if self.nested_cursor == len(self.list_of_list[self.cursor]):
+			self.cursor += 1
+			self.nested_cursor = 0
 		if self.cursor == len(self.list_of_list):
 			raise StopIteration
 		item = self.list_of_list[self.cursor][self.nested_cursor]
-		if self.nested_cursor == len(self.list_of_list[self.cursor]):
-			self.nested_cursor = -1
-			self.cursor += 1
+
+		# print(f'{self.cursor=} {self.nested_cursor=}')
 		return item
 
 
